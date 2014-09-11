4e64520326409c1d01549745860d5244eac3b456
kodered <blogger.saurav@gmail.com>
2014-09-11 16:02:25 +0530

    Commiting Set Timers Dialog for SMS

--- src/com/example/gps_test2/Settings_View.java
+++ src/com/example/gps_test2/Settings_View.java
@@ -1,16 +1,24 @@
 package com.example.gps_test2;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class Settings_View extends ListActivity
 {
+		TimePicker pickerTime;
 
 		public void onCreate(Bundle icicle)
 		{
@@ -23,8 +31,44 @@ public class Settings_View extends ListActivity
 		  protected void onListItemClick(ListView l, View v, int position, long id) {
 			    String item = (String) getListAdapter().getItem(position);
 			    if(item.equalsIgnoreCase("Set Timers"))
-			    {
-			    	Toast.makeText(this, "Set Timers", Toast.LENGTH_SHORT).show();
+			    {	
+			    	
+			    	//Toast.makeText(this, "Set Timers", Toast.LENGTH_SHORT).show();
+			    	
+			    	final CharSequence[] items={"10 Mins","20 Mins","30 Mins"};
+			    	
+			    	AlertDialog.Builder builder=new AlertDialog.Builder(this);
+			    	builder.setIcon(R.drawable.sms);
+			    	builder.setTitle("Set Interval");
+			    	//builder.setMessage("This interval determines the timegap while sending SMS to the selected contacts");
+			    	builder.setPositiveButton("Set", new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {}
+					});
+			    
+			    	builder.setSingleChoiceItems(items,-1, new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub	
+							if("10 Secs".equals(items[which]))
+							{
+							 Toast.makeText(getBaseContext(), "10 Mins Selected", Toast.LENGTH_SHORT).show();
+							}
+							else if("20 Secs".equals(items[which]))
+							{
+								 Toast.makeText(getBaseContext(), "20 Mins Selected", Toast.LENGTH_SHORT).show();
+							}
+							else if("30 Secs".equals(items[which]))
+							{
+								 Toast.makeText(getBaseContext(), "30 Mins Selected", Toast.LENGTH_SHORT).show();
+							}
+							
+						}
+					});
+			    	builder.show();
+			    	
 			    }
 			    if(item.equalsIgnoreCase("Send Sms?"))
 			    {
